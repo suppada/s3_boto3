c6b87f053d7f2c24d8da7092daeb980f4ad6d3ed
suresh <suppada6311@ucumberlands.edu>
2021-07-26 06:43:02 -0400

    s3_functions.py and calling_functions.py file

---
+++ s3_bucket/calling_functions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+from botocore.retries import bucket
+from s3_functions import create_bucket, upload_file, delete_obj, delete_bucket
+
+bucketname = 'suresh-test-3'
+try:
+    create_bucket(bucket)
+    upload_file(bucket)
+except Exception as e:
+    print(e)
+
+try:
+    delete_obj(bucket)
+    delete_bucket(bucket)
+except Exception as d:
+    print (d)
--- s3_bucket/s3_functions.py
+++ s3_bucket/s3_functions.py
@@ -11,46 +11,31 @@ Account_id = ''
 file_name = ''
 
 def create_bucket(bucket):
-    try:
-        response = s3.create_bucket(
-            ACL='private',
-            Bucket=(bucketname),
-            CreateBucketConfiguration={
-                'LocationConstraint': 'us-east-2'
-        },
-        ObjectLockEnabledForBucket=False
-        )
-    except Exception as e:
-        print (e)
-create_bucket(bucket)
+    response = s3.create_bucket(
+        ACL='private',
+        Bucket=(bucketname),
+        CreateBucketConfiguration={
+            'LocationConstraint': 'us-east-2'
+    },
+    ObjectLockEnabledForBucket=False
+    )
+
 
 def upload_file(bucket):
-    try:
-        with open((file_name), 'rb') as data:
-            upload =  s3.upload_fileobj(data, (bucketname), (file_name))
-            return upload
-    except Exception as e:
-        print (e)
-upload_file(bucket)
+    with open((file_name), 'rb') as data:
+        upload =  s3.upload_fileobj(data, (bucketname), (file_name))
+    return upload
 
 def delete_obj(bucket):
-    try:
-        response = s3.delete_object(
-            Bucket=(bucketname),
-            Key=(file_name),
-        )
-        print(response)
-    except Exception as e:
-        print (e)
-delete_obj(bucket)
+    response = s3.delete_object(
+        Bucket=(bucketname),
+        Key=(file_name),
+    )
+    print(response)
 
 def delete_bucket(bucket):
-    try:
-        response = s3.delete_bucket(
-            Bucket=(bucketname),
-            ExpectedBucketOwner=(Account_id)
-        )
-        print(response)
-    except Exception as d:
-        print (d)
-delete_bucket(bucket)
+    response = s3.delete_bucket(
+        Bucket=(bucketname),
+        ExpectedBucketOwner=(Account_id)
+    )
+    print(response)
